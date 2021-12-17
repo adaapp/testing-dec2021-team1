d6ee20dbc0b931207bc51f9dcce58ca51be1675b
01jondaniel <90607595+01jondaniel@users.noreply.github.com>
2021-12-17 14:34:05 +0000

    Update src/App.test.js
    Co-authored-by: Katia Punter <katiapunter@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hex 2 RGB/i);
+  const titleElement = screen.getByText(/Hex 2 RGB/i);
   expect(linkElement).toBeInTheDocument();
   expect(linkElement).toHaveClass('App-Title');
 

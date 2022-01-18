96e7810741d8f3a160229536fb18207472cce04a
01jondaniel <jonathandaniel.01@outlook.com>
2022-01-18 17:32:57 +0000

    Solved the missing #

---
+++ src/conversionFunctions.test.js
@@ -0,0 +1,17 @@
+import './conversionFunctions';
+const {
+  hexconverter,
+  rgbconverter
+} = require("./conversionFunctions");
+
+describe('Test the hexconverter function ', () => {
+  test('Inputting a hex value will give you a rgb value', () => {
+      expect(hexconverter('#8a3be2')).toBe("rgb(138, 59, 226)");
+  })
+}); 
+
+describe('Test the hexconverter function ', () => {
+  test('Inputting a hex value will give you a rgb value', () => {
+      expect(hexconverter('#8a3be2')).toBe("rgb(138, 59, 226)");
+  })
+}); 
